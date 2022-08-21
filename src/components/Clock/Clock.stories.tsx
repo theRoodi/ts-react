8689bf8b64b93432e37828ea1f8c6d2e8e720101
roodi <igorrud95@gmail.com>
2022-08-21 23:49:50 +0700

    useEffect

--- src/components/Clock/Clock.stories.tsx
+++ src/components/Clock/Clock.stories.tsx
@@ -1,9 +1,57 @@
 import {Clock} from './Clock';
+import {useEffect, useState} from 'react';
 
 export default {
 title: "Clock"
 }
 
-export const BaseExample = () => {
-    return< Clock />
+export const BaseAnalogExample = () => {
+    return< Clock mode={'analog'}/>
+}
+
+export const BaseDigitalExample = () => {
+    return< Clock mode={'digital'}/>
+}
+
+
+export const ResetEffectExample = () => {
+
+    const [counter, setCounter] = useState(1)
+
+    console.log('render')
+
+    useEffect(()=>{
+        console.log("effect")
+    }, [])
+
+    const onIncrease = () => {
+        setCounter(counter + 1)
+    }
+
+    return<>
+        Hello, counter: {counter} <button onClick={onIncrease}>+</button>
+    </>
+}
+
+export const KeyTrackerExample = () => {
+
+    const [key, setKey] = useState('')
+
+    console.log('render')
+
+    useEffect(()=>{
+        console.log("effect")
+        const handler = (e: KeyboardEvent) => {
+            setKey(key + e.key)
+        }
+        window.addEventListener('keypress', handler)
+
+        return () => {
+            window.removeEventListener('keypress', handler)
+        }
+    }, [key])
+
+    return<>
+        Key: {key}
+    </>
 }
--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,10 +1,12 @@
-import {useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import styles from './AnalogClock.module.css'
 
-type PropsType ={
-
+type PropsType = {
+    mode: 'analog' | 'digital'
 }
 
-export const Clock: React.FC<PropsType> = () => {
+const getTwoDigits = (num: number) => num < 10 ? `0${num}` : num
+export const Clock: React.FC<PropsType> = (props) => {
     const [date, setDate] = useState(new Date())
 
     useEffect(() => {
@@ -15,18 +17,61 @@ export const Clock: React.FC<PropsType> = () => {
         return () => {
             clearInterval(intervalID)
         }
-    },[])
+    }, [])
 
-    const getTwoDigits = (num: number) => num < 10 ? `0${num}` : num
 
+    let view;
+
+    switch (props.mode) {
+        case 'analog':
+            view = <AnalogClock date={date}/>
+            break
+        case 'digital':
+        default:
+            view = <DigitalClock date={date}/>
+            break
+    }
+
+    return (
+        <div>
+            {view}
+        </div>
+    )
+}
+
+export type ClockViewPropsType = {
+    date: Date
+}
+
+export const DigitalClock: React.FC<ClockViewPropsType> = ({date}) => {
     const hours = getTwoDigits(date.getHours())
     const minutes = getTwoDigits(date.getMinutes())
     const seconds = getTwoDigits(date.getSeconds())
 
 
     return (
-        <div>
-            <span>{hours}</span> : <span>{minutes}</span> : <span>{seconds}</span>
+        <><span>{hours}</span> : <span>{minutes}</span> : <span>{seconds}</span></>
+    )
+}
+
+export const AnalogClock: React.FC<ClockViewPropsType> = ({date}) => {
+
+    const secondsStyle = {
+        transform: `rotate(${date.getSeconds() * 6}deg)`
+    };
+    const minutesStyle = {
+        transform: `rotate(${date.getMinutes() * 6}deg)`
+    };
+    const hoursStyle = {
+        transform: `rotate(${date.getHours() * 30}deg)`
+    };
+    return (
+        <div className={styles.clock}>
+            <div className={styles['analog-clock']}>
+                <div className={`${styles.dial} ${styles.seconds}`} style={secondsStyle}/>
+                <div className={`${styles.dial} ${styles.minutes}`} style={minutesStyle}/>
+                <div className={`${styles.dial} ${styles.hours}`} style={hoursStyle}/>
+            </div>
         </div>
     )
 }
